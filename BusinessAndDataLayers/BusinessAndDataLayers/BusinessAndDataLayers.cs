da06682bbf88e8a6c227087f7d9c31a20a151d39
Christian Findlay <a@a.com>
2020-07-15 09:40:48 +1000

    Comment

--- BusinessAndDataLayers/BusinessAndDataLayers/BusinessAndDataLayers.cs
+++ BusinessAndDataLayers/BusinessAndDataLayers/BusinessAndDataLayers.cs
@@ -7,6 +7,10 @@ using System.Threading.Tasks;
 namespace BusinessAndDataLayers
 {
     #region Interfaces
+    /// <summary>
+    /// This is a modified repository. It is not the standard DDD version of a repository
+    /// Note: Transaction is left off these methods, but a transaction will probably need to be passed around so that database calls can access the transaction. It could be IDbTransaction, or a new interface like IDbTransaction
+    /// </summary>
     public interface IRepository<T>
     {
         Task DeleteAsync(Guid key);
