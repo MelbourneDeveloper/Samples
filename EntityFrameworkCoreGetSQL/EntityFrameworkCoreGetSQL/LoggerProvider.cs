2e871cf195c9cb23b71690c10e589af5b6ec06ae
Christian Findlay <a@a.com>
2020-07-19 15:28:52 +1000

    Add the EF core SQL sample

---
+++ EntityFrameworkCoreGetSQL/EntityFrameworkCoreGetSQL/LoggerProvider.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace EntityFrameworkCoreGetSQL
+{
+    public class LoggerProvider : ILoggerProvider
+    {
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new Logger();
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ EntityFrameworkCoreGetSQL/EntityFrameworkCoreGetSQL/OrderLine.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EntityFrameworkCoreGetSQL
+{
+    public class OrderLine
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public Guid Item { get; set; }
+        public int Count { get; set; }
+    }
+}
