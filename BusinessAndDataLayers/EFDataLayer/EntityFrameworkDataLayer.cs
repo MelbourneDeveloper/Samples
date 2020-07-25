cc8d10cb26097fe2acb18c2550f5e94ee0f76d78
Christian Findlay <a@a.com>
2020-07-26 08:51:47 +1000

    EF works

--- BusinessAndDataLayers/EFDataLayer/EntityFrameworkDataLayer.cs
+++ BusinessAndDataLayers/EFDataLayer/EntityFrameworkDataLayer.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace BusinessAndDataLayers
 {
-    public class EntityFrameworkDataLayer 
+    public class EntityFrameworkDataLayer
     {
         DbContext _dbContext;
 
@@ -23,19 +23,33 @@ namespace BusinessAndDataLayers
 
         public Task<IAsyncEnumerable<object>> GetAsync(Expression predicate)
         {
-            throw new NotImplementedException();
-            /*
+            //Get the entity type from the predicate
+            var type = predicate.Type.GenericTypeArguments[0];
+
+            //Get the set method
             var setMeth = typeof(DbContext).GetMethod(nameof(DbContext.Set), new Type[] { });
 
-            var setMethodWithTypeArgument = setMeth.MakeGenericMethod(new Type[] { typeof(T) });
+            var setMethodWithTypeArgument = setMeth.MakeGenericMethod(new Type[] { type });
 
-            var dbSets = (IQueryable<T>)setMethodWithTypeArgument.Invoke(_dbContext, null);
+            //Get the DbSet
+            var dbSet = (IQueryable)setMethodWithTypeArgument.Invoke(_dbContext, null);
 
-            var whereMethod = typeof(Queryable).GetMethod(nameof(Queryable.Where), new Type[] { });
+            //Warning extremely dodgy
+            //TODO: Fix
+            var whereMethod = typeof(Queryable).GetMethods().Where(m => m.Name == "Where").First().MakeGenericMethod(type);
 
-            return Task.FromResult(Queryable.Where(dbSets, predicate).ToAsyncEnumerable());
+            //Invoke where
+            var result = whereMethod.Invoke(null, new object[] { dbSet, predicate });
 
-            */
+            //Warning: More dodge here
+            //Cast to enumerable of objects
+            var castMethod = typeof(Queryable).GetMethods().Where(m => m.Name == "Cast").First().MakeGenericMethod(typeof(object));
+
+            //Cast to IEnumerable<object>
+            var enumerableObjects = (IEnumerable<object>)castMethod.Invoke(null, new object[] { result });
+
+            //Conver to async and return
+            return Task.FromResult(enumerableObjects.ToAsyncEnumerable());
         }
 
         public Task<object> SaveAsync(object item, bool isUpdate)
