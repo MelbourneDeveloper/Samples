398777900708a6930119dac2c1a65d7f9e9811d2
Christian Findlay <a@a.com>
2020-08-23 16:36:53 +1000

    Add the uno template

---
+++ UnoCrossPlatformTemplate/UnoCrossPlatform.Wasm/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace UnoCrossPlatform.Wasm
+{
+    public class Program
+    {
+        private static App _app;
+
+        private static int Main(string[] args)
+        {
+            Windows.UI.Xaml.Application.Start(_ => _app = new App());
+
+            return 0;
+        }
+    }
+}
---
+++ UnoCrossPlatformTemplate/UnoCrossPlatform.macOS/Main.cs
@@ -0,0 +1,15 @@
+﻿using AppKit;
+
+namespace UnoCrossPlatform.macOS
+{
+    internal static class MainClass
+    {
+        private static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.SharedApplication.Delegate = new App();
+            NSApplication.Main(args);
+        }
+    }
+}
+
