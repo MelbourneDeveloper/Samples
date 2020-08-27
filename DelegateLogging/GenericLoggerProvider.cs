1cb984661e62b61bc28a86f479577d934e6d666a
Christian Findlay <a@a.com>
2020-08-27 18:13:40 +1000

    Add the first version of delegate logging

---
+++ DelegateLogging/GenericLoggerProvider.cs
@@ -0,0 +1,29 @@
+﻿
+
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace DelegateLogging
+{
+    public class GenericLoggerProvider : ILoggerProvider
+    {
+        private Func<string, ILogger>? _createLogger;
+
+        public GenericLoggerProvider(Func<string, ILogger> createLogger)
+        {
+            _createLogger = createLogger;
+        }
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            _createLogger?.Invoke(categoryName);
+
+            throw new InvalidOperationException();
+        }
+
+#pragma warning disable CA1816 // Dispose methods should call SuppressFinalize
+        public void Dispose() => _createLogger = null;
+#pragma warning restore CA1816 // Dispose methods should call SuppressFinalize
+    }
+}
+
