3bc38d75fb990e94c27bd5d48c2de90b50ba2012
Christian Findlay <a@a.com>
2020-09-05 09:18:04 +1000

    Get the query basically working with lite db

--- BusinessAndDataLayers/LiteDBLib/LiteDbDataLayer.cs
+++ BusinessAndDataLayers/LiteDBLib/LiteDbDataLayer.cs
@@ -1,5 +1,6 @@
 ﻿using LiteDB;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
@@ -21,20 +22,47 @@ namespace LiteDBLib
             throw new NotImplementedException();
         }
 
-        public Task<IAsyncEnumerable<T>> GetAsync<T>(Expression<Func<T, bool>> predicate)
+        public Task<IAsyncEnumerable<object>> GetAsync(Expression predicate)
         {
+            var recordType = predicate.Type.GenericTypeArguments[0];
 
-            // Get a collection (or create, if doesn't exist)
-            var orders = _db.GetCollection<T>("OrderRecords");
+            var getCollectionMethod = typeof(LiteDatabase).GetMethod(nameof(LiteDatabase.GetCollection), new Type[] { }).MakeGenericMethod(new Type[] { recordType });
 
-            // Use LINQ to query documents (filter, sort, transform)
-            var results = orders.Query()
-                .Where(predicate)
-                .ToList();
+            var queryMethod = typeof(ILiteCollection<>).MakeGenericType(new Type[] { recordType }).GetMethod(nameof(ILiteCollection<object>.Query));
 
-            return Task.FromResult(results.ToAsyncEnumerable());
+            //TODO: this is pretty horrible
+            var whereMethod = Enumerable.FirstOrDefault(typeof(LiteQueryable<>).MakeGenericType((Type[])(new Type[] { recordType })).GetMethods(), m =>
+             {
+                 var parameters = m.GetParameters();
 
+                 var firstParameter = parameters.First();
 
+                 if (firstParameter.ParameterType.Name != "Expression`1")
+                 {
+                     return false;
+                 }
+
+                 return m.Name == nameof(LiteQueryable<object>.Where);
+             });
+
+            var toListMethod = typeof(LiteQueryable<>).MakeGenericType(new Type[] { recordType }).GetMethod(nameof(LiteQueryable<object>.ToList));
+
+            //Get the collection
+            var liteCollection = getCollectionMethod.Invoke(_db, null);
+
+            //Get the queryable
+            var liteQueryable = queryMethod.Invoke(liteCollection, null);
+
+            //Get queryable with a where clause
+            var liteQueryableWithWhere = whereMethod.Invoke(liteQueryable, new object[] { predicate });
+
+            //Perform the query
+            var list = (IList)toListMethod.Invoke(liteQueryableWithWhere, null);
+
+            //Cast to a list of objects
+            var objects = list.Cast<object>();
+
+            return Task.FromResult(objects.ToAsyncEnumerable());
         }
 
 
