133b8cfaf1576b9ae2e9740eac5b50ff69f8401d
Christian Findlay <a@a.com>
2020-09-13 20:05:24 +1000

    Add the extensions

--- BusinessAndDataLayers/BusinessLayer/IBusinessLayer.cs
+++ BusinessAndDataLayers/BusinessLayer/IBusinessLayer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
 namespace BusinessLayerLib
---
+++ BusinessAndDataLayers/BusinessLayer/ServiceCollectionExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace BusinessLayerLib
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection OnSaving<T>(this IServiceCollection serviceCollection, Saving<T> saving) => serviceCollection.AddSingleton(saving);
+
+    }
+}
