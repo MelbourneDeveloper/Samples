1652e7e6a2c9355f58815d060016058e1d7f5465
Christian Findlay <a@a.com>
2020-10-17 15:05:30 +1100

    Add a sample for F# from C#

---
+++ F#/FSharpFromCSharp/FSharpFromCSharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.FSharp.Collections;
+using System;
+
+namespace FSharpFromCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = FSharpList<int>.Cons(
+            1,
+            FSharpList<int>.Cons(2,
+                FSharpList<int>.Cons(
+                    3,
+                    FSharpList<int>.Empty)));
+
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}
