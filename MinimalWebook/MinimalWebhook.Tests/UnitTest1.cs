a7ed0884aef29975ddcd0a648d3f45002b99d672
Christian Findlay <16697547+MelbourneDeveloper@users.noreply.github.com>
2023-04-02 15:28:06 +1000

    Add the minimal webhook sample

---
+++ MinimalWebook/MinimalWebhook.Tests/UnitTest1.cs
@@ -0,0 +1,88 @@
+﻿//This is an ASP .NET Core Integration test that waits for a webhook and verifies that
+//the service is correctly receiving the POST data
+
+using System.Net;
+using System.Text.Json;
+using System.Xml;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Xunit;
+using MinimalWebhook;
+
+namespace MinimalWebhook.Tests;
+
+/// <summary>
+/// This is a fake implementation of IReceiveWebhook so we can keep track
+/// of incoming webhook Calls
+/// </summary>
+public class FakewebhookReceiver : IReceiveWebhook
+{
+    public List<String> Receipts = new List<String>();
+
+    public async Task<string> ProcessRequest(String requestBody)
+    {
+        Receipts.Add(requestBody);
+
+        return "Hello back"; 
+    }
+}
+
+public class Tests
+{
+[Fact]
+public async Task TestReceivingWebhook()
+{
+    var fakeReceiver = new FakewebhookReceiver();
+
+    await WithTestServer(async (c, s) =>
+    {
+        //Wait 100 seconds to receive the webhook
+        for (var i = 0; i < 100; i++)
+        {
+            //----------------------------------------
+            //This emulates the webhook receipt locally. Delete this and use ngrok to tunnel the call through
+            var response = await c.PostAsync("webhook", new StringContent("Hi"));
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            var responseText = await response.Content.ReadAsStringAsync();
+
+            Assert.Equal("Hello back", responseText);
+            //----------------------------------------
+
+            if (fakeReceiver.Receipts.Count > 0)
+            {
+                break;
+            }
+
+            await Task.Delay(1000);
+        }
+
+        //Verify that we received the correct details from the webhook
+        Assert.Equal("Hi", fakeReceiver.Receipts.First());
+
+
+
+    }, s => s.AddSingleton((IReceiveWebhook)fakeReceiver));
+}
+
+/// <summary>
+/// Spins up a test server and allows us to configure the server with test doubles
+/// </summary>
+private async Task WithTestServer(
+    Func<HttpClient, IServiceProvider, Task> test,
+    Action<IServiceCollection> configureServices)
+{
+    await using var application =
+        new WebApplicationFactory<Program>().
+        WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureTestServices(services => configureServices(services));
+        });
+
+    using var client = application.CreateClient();
+    await test(client, application.Services);
+}
+}
---
+++ MinimalWebook/MinimalWebhook/Program.cs
@@ -0,0 +1,52 @@
+﻿//This is an ASP .NET Core Minimal API that receives Webhook POST data
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Configure services
+builder.Services.AddSingleton<IReceiveWebhook, ConsoleWebhookReceiver>();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+//Listen for POST webhooks
+app.MapPost("/webhook", async (HttpContext context) =>
+{
+    var receiveWebook = context.RequestServices.GetRequiredService<IReceiveWebhook>();
+
+    return await receiveWebook.ProcessRequest(
+        Encoding.UTF8.GetString((await context.Request.BodyReader.ReadAsync()).Buffer));
+
+});
+
+app.Run();
+
+/// <summary>
+/// An abstraction to process the POST request data that the webbook receives.
+/// </summary>
+public interface IReceiveWebhook
+{
+    Task<string> ProcessRequest(string requestBody);
+}
+
+/// <summary>
+/// An implementation for IReceiveWebhook that writes to the console
+/// </summary>
+public class ConsoleWebhookReceiver : IReceiveWebhook
+{
+    /// <summary>
+    /// Writes the POST request body to the console and returns JSON
+    /// </summary>
+    public async Task<String> ProcessRequest(string requestBody)
+    {
+        Console.WriteLine($"Request Body: {requestBody}");
+
+        return "{\"message\" : \"Thanks! We got your webhook\"}";
+    }
+}
+
+/// <summary>
+/// This is just a requirement for ASP .NET Core and testing this service
+/// </summary>
+public partial class Program { }
