ffe853fb5decf04fc46d0fccbb76867a2a1540ee
Christian Findlay <16697547+MelbourneDeveloper@users.noreply.github.com>
2024-01-24 15:35:20 +1100

    kinda working

--- MinimalAOT/MinimalAOT/Migrations/20240124030116_InitialCreate.cs
+++ MinimalAOT/MinimalAOT/Migrations/20240124030116_InitialCreate.cs
@@ -13,25 +13,27 @@ namespace MinimalAOT.Migrations
         {
             migrationBuilder.CreateTable(
                 name: "Todos",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "INTEGER", nullable: false)
-                        .Annotation("Sqlite:Autoincrement", true),
-                    Title = table.Column<string>(type: "TEXT", nullable: true),
-                    DueBy = table.Column<DateOnly>(type: "TEXT", nullable: true),
-                    IsComplete = table.Column<bool>(type: "INTEGER", nullable: false)
-                },
+                columns: table =>
+                    new
+                    {
+                        Id = table
+                            .Column<int>(type: "INTEGER", nullable: false)
+                            .Annotation("Sqlite:Autoincrement", true),
+                        Title = table.Column<string>(type: "TEXT", nullable: true),
+                        DueBy = table.Column<DateOnly>(type: "TEXT", nullable: true),
+                        IsComplete = table.Column<bool>(type: "INTEGER", nullable: false)
+                    },
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_Todos", x => x.Id);
-                });
+                }
+            );
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropTable(
-                name: "Todos");
+            migrationBuilder.DropTable(name: "Todos");
         }
     }
 }
--- MinimalAOT/MinimalAOT/Program.cs
+++ MinimalAOT/MinimalAOT/Program.cs
@@ -11,60 +11,70 @@ builder.Services.AddLogging(b => b.AddConsole());
 builder.Logging.AddConsole();
 builder.Services.AddProblemDetails();
 
-
 builder.Services.AddDbContext<MyDbContext>(options =>
-    options.UseSqlite("Data Source=mydatabase.db"));
-    
+    options.UseSqlite("Data Source=mydatabase.db")
+        .UseModel(YourProjectNamespace.MyDbContextModel.Instance));
+
 var app = builder.Build();
 app.UseMiddleware<RequestLoggingMiddleware>();
 
-
-app.UseStatusCodePages(async statusCodeContext 
-    => await Results.Problem(statusCode: statusCodeContext.HttpContext.Response.StatusCode)
-        .ExecuteAsync(statusCodeContext.HttpContext));
+app.UseStatusCodePages(
+    async statusCodeContext =>
+        await Results
+            .Problem(statusCode: statusCodeContext.HttpContext.Response.StatusCode)
+            .ExecuteAsync(statusCodeContext.HttpContext)
+);
 
 var todosApi = app.MapGroup("/todos");
-todosApi.MapGet("/", async (a) =>
-{
-    var myDbContext = new MyDbContext(new DbContextOptions<MyDbContext>());
-    await myDbContext.Todos.ToListAsync();
-});
-todosApi.MapPost("/", async (Todo todo) =>
-{
-    var myDbContext = new MyDbContext(new DbContextOptions<MyDbContext>());
-    myDbContext.Todos.Add(todo);
-    await myDbContext.SaveChangesAsync();
-});
-
+todosApi.MapGet(
+    "/",
+    async (a) =>
+    {
+        var myDbContext = new MyDbContext(new DbContextOptions<MyDbContext>());
+        await myDbContext.Todos.ToListAsync();
+    }
+);
+todosApi.MapPost(
+    "/",
+    async (Todo todo,  MyDbContext myDbContext) =>
+    {
+        try
+        {
+            myDbContext.Todos.Add(todo);
+            await myDbContext.SaveChangesAsync();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+);
 
 app.Run();
-
 
 class MyDbContext : DbContext
 {
     public MyDbContext(DbContextOptions<MyDbContext> options)
-        : base(options)
-    {
-    }
+        : base(options) { }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         if (!optionsBuilder.IsConfigured)
         {
-            optionsBuilder.UseSqlite("Data Source=mydatabase.db").UseModel(YourProjectNamespace.MyDbContextModel.Instance);
+            optionsBuilder
+                .UseSqlite("Data Source=mydatabase.db")
+                .UseModel(YourProjectNamespace.MyDbContextModel.Instance);
         }
     }
-    
+
     public DbSet<Todo> Todos { get; set; }
 }
 
 public record Todo(int Id, string? Title, DateOnly? DueBy = null, bool IsComplete = false);
 
 [JsonSerializable(typeof(Todo[]))]
-internal partial class AppJsonSerializerContext : JsonSerializerContext
-{
-}
-
+internal partial class AppJsonSerializerContext : JsonSerializerContext { }
 
 public class RequestLoggingMiddleware
 {
@@ -79,14 +89,18 @@ public class RequestLoggingMiddleware
 
     public async Task Invoke(HttpContext context)
     {
-        _logger.LogInformation($"Handling request: {context.Request.Method} {context.Request.Path}");
+        _logger.LogInformation(
+            $"Handling request: {context.Request.Method} {context.Request.Path}"
+        );
         try
         {
             await _next(context); // Call the next delegate/middleware in the pipeline
         }
         finally
         {
-            _logger.LogInformation($"Finished handling request. Status Code: {context.Response.StatusCode}");
+            _logger.LogInformation(
+                $"Finished handling request. Status Code: {context.Response.StatusCode}"
+            );
         }
     }
 }
