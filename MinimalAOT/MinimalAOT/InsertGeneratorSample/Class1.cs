3020109f333788ccd55fdb6b5a83caf3a651466e
Christian Findlay <16697547+MelbourneDeveloper@users.noreply.github.com>
2024-02-20 18:45:01 +1100

    a few fixes

--- MinimalAOT/MinimalAOT/InsertGeneratorSample/Class1.cs
+++ MinimalAOT/MinimalAOT/InsertGeneratorSample/Class1.cs
@@ -1,11 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace InsertGeneratorSample;
+﻿namespace InsertGeneratorSample;
 
 
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, Inherited = false, AllowMultiple = false)]
 public sealed class SourceGenAttribute : Attribute
 {
+ 
 }
 
 [SourceGen]
--- MinimalAOT/MinimalAOT/InsertUpdateGenerator/SqlInsertSourceGenerator.cs
+++ MinimalAOT/MinimalAOT/InsertUpdateGenerator/SqlInsertSourceGenerator.cs
@@ -1,51 +1,55 @@
-﻿namespace InsertUpdateGenerator;
-
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.Text;
-using System.Text;
-
-[Generator]
-public class SqlInsertSourceGenerator : ISourceGenerator
+namespace InsertUpdateGenerator
 {
-    public void Initialize(GeneratorInitializationContext context)
-    {
-        // Register a syntax receiver that will be created for each compilation
-        context.RegisterForSyntaxNotifications(() => new ModelSyntaxReceiver());
-    }
 
-    public void Execute(GeneratorExecutionContext context)
-    {
-        // Retrieve the populated receiver 
-        if (context.SyntaxReceiver is not ModelSyntaxReceiver receiver)
-            return;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+    using Microsoft.CodeAnalysis.Text;
+    using System.Text;
 
-        foreach (var classModel in receiver.CandidateClasses)
+    [Generator]
+    public class SqlInsertSourceGenerator : ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context)
         {
-            var source = GenerateInsertStatement(classModel);
-            context.AddSource($"{classModel.Identifier.Text}_InsertStatement.cs", SourceText.From(source, Encoding.UTF8));
-        }
-    }
-
-    private string GenerateInsertStatement(ClassDeclarationSyntax classModel)
-    {
-        var className = classModel.Identifier.ValueText;
-        var properties = classModel.Members.OfType<PropertyDeclarationSyntax>();
-
-        var columns = new StringBuilder();
-        var values = new StringBuilder();
-
-        foreach (var property in properties)
-        {
-            var propName = property.Identifier.ValueText;
-            columns.Append($"{propName}, ");
-            values.Append($"@{propName}, ");
+            // Register a syntax receiver that will be created for each compilation
+            context.RegisterForSyntaxNotifications(() => new ModelSyntaxReceiver());
         }
 
-        columns.Length -= 2; // Remove last comma
-        values.Length -= 2; // Remove last comma
+        public void Execute(GeneratorExecutionContext context)
+        {
+            // The context variable is available here
+            if (context.SyntaxReceiver is ModelSyntaxReceiver receiver)
+            {
+                foreach (var classModel in receiver.CandidateClasses)
+                {
+                    // Now you can report diagnostics or generate code
+                    context.ReportDiagnostic(Diagnostic.Create(new DiagnosticDescriptor("SG001", "Info", $"Processing class {classModel.Identifier.Text}", "SourceGenerator", DiagnosticSeverity.Info, isEnabledByDefault: true), Location.None));
 
-        return $@"
+                    var source = GenerateInsertStatement(classModel);
+                    context.AddSource($"{classModel.Identifier.Text}_InsertStatement.cs", SourceText.From(source, Encoding.UTF8));
+                }
+            }
+        }
+
+        private string GenerateInsertStatement(ClassDeclarationSyntax classModel)
+        {
+            var className = classModel.Identifier.ValueText;
+            var properties = classModel.Members.OfType<PropertyDeclarationSyntax>();
+
+            var columns = new StringBuilder();
+            var values = new StringBuilder();
+
+            foreach (var property in properties)
+            {
+                var propName = property.Identifier.ValueText;
+                columns.Append($"{propName}, ");
+                values.Append($"@{propName}, ");
+            }
+
+            columns.Length -= 2; // Remove last comma
+            values.Length -= 2; // Remove last comma
+
+            return $@"
 public partial class {className}
 {{
     public string GetInsertStatement()
@@ -54,20 +58,22 @@ public partial class {className}
     }}
 }}
 ";
-    }
+        }
 
-    class ModelSyntaxReceiver : ISyntaxReceiver
-    {
-        public List<ClassDeclarationSyntax> CandidateClasses { get; } = new();
-
-        public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+        class ModelSyntaxReceiver : ISyntaxReceiver
         {
-            // Look for classes with a specific attribute or naming convention
-            if (syntaxNode is ClassDeclarationSyntax classDeclaration &&
-                classDeclaration.AttributeLists.Count > 0)
+            public List<ClassDeclarationSyntax> CandidateClasses { get; } = new List<ClassDeclarationSyntax>();
+
+            public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+            {
+                // Look for classes with a specific attribute or naming convention
+                if (syntaxNode is ClassDeclarationSyntax classDeclaration &&
+                    classDeclaration.AttributeLists.Count > 0)
             {
                 CandidateClasses.Add(classDeclaration);
+                }
             }
         }
     }
+
 }
