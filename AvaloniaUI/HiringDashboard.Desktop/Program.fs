e7cbbf3d7aea7b34159f9839973347a0e9e49176
Max Katz <maxkatz6@outlook.com>
2024-12-20 22:43:50 -0800

    Recreate Browsre and Desktop projects from our templates

---
+++ AvaloniaUI/HiringDashboard.Desktop/Program.fs
@@ -0,0 +1,19 @@
+namespace HiringDashboard
+
+open System
+open Avalonia
+open HiringDashboard
+
+module Program =
+
+    [<CompiledName "BuildAvaloniaApp">] 
+    let buildAvaloniaApp () = 
+        AppBuilder
+            .Configure<App>()
+            .UsePlatformDetect()
+            .WithInterFont()
+            .LogToTrace(areas = Array.empty)
+
+    [<EntryPoint; STAThread>]
+    let main argv =
+        buildAvaloniaApp().StartWithClassicDesktopLifetime(argv)
